a62b0d34432ba67a1c2256f5ce04e5c0338ea84c
Rodrigo Davies <rodrigo.davies@gmail.com>
2014-10-19 01:10:18 -0700

    Adds reverse geocoder script

---
+++ revgeocoder.py
@@ -0,0 +1,64 @@
+# A simple reverse geocoding script for working with csv files, based on the Geocoder library.
+# Rodrigo Davies
+
+# Change the headers and columns to fit the dataset you're working with.
+
+from pygeocoder import Geocoder
+import csv
+import sys
+from time import sleep
+
+# initialize and set proxy
+myGeo = Geocoder()
+myGeo.set_proxy('') # fill in if you're using one
+
+def cleanid(col):
+	clean_string = str(col).split("'")
+	return int(clean_string[1])
+
+def cleanGeo(col):
+	clean_string = str(col).split("'")
+	return float(clean_string[1])
+
+def checkNone(result):
+	if result == None:
+		result = ''
+	return result
+
+def revGeo(sourceFile):
+	with open(sourceFile, 'rU') as csvfile:
+		dataString = ""
+		soup = csv.reader(csvfile, delimiter=',')
+		row_num = 1
+		for row in soup:
+			cols = str(row)
+			cols = cols.split(',')
+			ks_id = cleanid(cols[0])
+			lat = cleanGeo(cols[1])
+			lon = cleanGeo(cols[2])
+			result = myGeo.reverse_geocode(float(lat),float(lon))
+			latlon = str(lat) + "," + str(lon)
+			zipcode = checkNone(result[0].postal_code)
+			if len(str(zipcode)) == 4:
+				zipcode = '0' + zipcode #correcting for five digit zips
+			country = checkNone(result[0].country)
+			state = checkNone(result[0].state)
+			city = checkNone(result[0].city)
+			resultString = "%s\t%s\t%s\t%s\t%s\t%s\n"%(ks_id,latlon,zipcode,country,state,city)
+			dataString = "%s%s"%(dataString, resultString)
+			print "Done: (" + row_num +") " + zipcode + " " + country + " " + state + " " + city
+			row_num += 1
+			sleep(0.4)
+	filename = "revgeocoded-%s.tsv"%(sourceFile) #add tag to filename
+	writeData(dataString, filename)
+
+def writeData(dataString, file):
+	headerString = "id\tlatlon\tzip\tcountry\tstate\tcity\n"
+	totalString = "%s%s"%(headerString,dataString)
+	f = open(file, 'w')
+	f.write(totalString)
+	f.close()
+
+if __name__ == "__main__":
+    source = sys.argv[1]
+    revGeo(source)
